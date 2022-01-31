6766e5ecedeeb0b89ee5fd17386794722e934bc4
Matthias Geisler <matthias.geisler@data4life.care>
2022-01-31 22:24:12 +0100

    Bump Updates

--- gradlePlugin/test-utils-dependency/src/main/kotlin/tech/antibytes/gradle/util/test/dependency/Version.kt
+++ gradlePlugin/test-utils-dependency/src/main/kotlin/tech/antibytes/gradle/util/test/dependency/Version.kt
@@ -14,7 +14,7 @@ object Version {
         /**
          * [AnitBytes GradlePlugins](https://github.com/bitPogo/gradle-plugins)
          */
-        const val antibytes = "4b4554a"
+        const val antibytes = "74f873c"
 
         /**
          * [Spotless](https://plugins.gradle.org/plugin/com.diffplug.gradle.spotless)
