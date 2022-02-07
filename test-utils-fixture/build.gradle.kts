7186c2a2713f52ceb171adb2fb5a5211c39206bb
Matthias Geisler <matthias.geisler@data4life.care>
2022-02-07 10:40:23 +0100

    Add iOS to fixture

--- test-utils-fixture/build.gradle.kts
+++ test-utils-fixture/build.gradle.kts
@@ -36,6 +36,8 @@ kotlin {
 
     jvm()
 
+    ios()
+
     linuxX64()
 
     sourceSets {
@@ -114,6 +116,18 @@ kotlin {
             }
         }
 
+        val darwinMain by creating {
+            dependencies {
+                dependsOn(nativeMain)
+            }
+        }
+
+        val darwinTest by creating {
+            dependencies {
+                dependsOn(nativeTest)
+            }
+        }
+
         val otherMain by creating {
             dependencies {
                 dependsOn(nativeMain)
@@ -135,6 +149,18 @@ kotlin {
         val linuxX64Test by getting {
             dependencies {
                 dependsOn(otherTest)
+            }
+        }
+
+        val iosMain by getting {
+            dependencies {
+                dependsOn(darwinMain)
+            }
+        }
+
+        val iosTest by getting {
+            dependencies {
+                dependsOn(darwinTest)
             }
         }
     }
