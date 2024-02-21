474a76d12dc2742dae093f1610d85e85c32bc60c
Matthias Geisler <bitpogo@antibytes.tech>
2024-02-21 19:44:11 +0100

    Bump to d067446

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ pluginManagement {
 }
 
 plugins {
-    id("tech.antibytes.gradle.dependency.settings") version "ae101dc"
+    id("tech.antibytes.gradle.dependency.settings") version "d067446"
 }
 
 includeBuild("setup")
